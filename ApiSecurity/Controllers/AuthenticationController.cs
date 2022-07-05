c7f6e471908b7d8ede34dc87fa0b1cb7cdb2fedf
Jyotirmoy M <38112743+jmaschatak1@users.noreply.github.com>
2022-07-06 00:30:20 +0530

    Added custom token generation methods

---
+++ ApiSecurity/Controllers/AuthenticationController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace ApiSecurity.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthenticationController : ControllerBase
+{
+    private readonly IConfiguration _config;
+
+    public AuthenticationController(IConfiguration config)
+    {
+        _config = config;
+    }
+    public record AuthenticationData(string? UserName, string? Password);
+    public record UserData(int UserId, string UserName);
+
+    // api/authentication/token
+    [HttpPost("token")]
+    public ActionResult<string> Authenticate([FromBody] AuthenticationData data)
+    {
+        var user = ValidateCredentials(data);
+        if(user is null)
+        {
+            return Unauthorized();
+        }
+
+        var token = GenerateToken(user);
+        return Ok(token);
+    }
+    private string GenerateToken(UserData user)
+    {
+        var secretKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(
+            _config.GetValue<string>("Authentication:SecretKey")));
+
+        var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+        List<Claim> claims = new List<Claim>();
+        claims.Add(new Claim(JwtRegisteredClaimNames.Sub, user.UserId.ToString()));
+        claims.Add(new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName.ToString()));
+
+        var token = new JwtSecurityToken(
+            _config.GetValue<string>("Authentication:Issuer"),
+            _config.GetValue<string>("Authentication:Audience"),
+            claims,
+            DateTime.UtcNow,
+            DateTime.UtcNow.AddMinutes(1),
+            signingCredentials);
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+
+    private UserData? ValidateCredentials(AuthenticationData data)
+    {
+        if(CompareValues(data.UserName,"jyoti") &&
+            CompareValues(data.Password,"secret"))
+        {
+            return new UserData(1, data.UserName!);
+        }
+
+        if (CompareValues(data.UserName, "diego") &&
+            CompareValues(data.Password, "tequila"))
+        {
+            return new UserData(2, data.UserName!);
+        }
+        return null;
+    }
+
+    private bool CompareValues(string? actual, string expected)
+    {
+        if(actual is not null)
+        {
+            if(actual.Equals(expected, StringComparison.InvariantCultureIgnoreCase))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
